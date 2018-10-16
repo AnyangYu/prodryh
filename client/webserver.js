892ac84cae4f3e38dd17e409a0b964919a3a5828
AnyangYu <251581745@qq.com>
2018-10-17 01:23:34 +0800

    add simple web server

---
+++ client/webserver.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, './dist')));
+
+app.use('/', (req, res)=>{
+    var html = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf-8');
+    res.send(html);   
+})
+
+
+app.listen(81,() => {
+    console.log('app listening on port 80.');
+})
+
